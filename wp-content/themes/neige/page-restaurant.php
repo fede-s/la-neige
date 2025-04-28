4c1696fff97b8e4b7cb4ecf8be586639691721d2
DESKTOP-FTRQVEA\Tinma <xtinmax92@gmail.com>
2025-04-28 05:06:31 -0300

    opening hours added

--- wp-content/themes/neige/page-restaurant.php
+++ wp-content/themes/neige/page-restaurant.php
@@ -25,17 +25,25 @@ get_header(); ?>
       <div class="side-text">
          <h5><?= get_field('restaurant_pre_title'); ?></h5>
          <h1><?= get_field('restaurant_title'); ?></h1>
-         <div class="signature">
-            <?= file_get_contents(get_field('restaurant_signature')); ?>
-         </div>
-         <div class="shadow-image">
-            <div class="front">
-               <?= Utils::imgLazy(get_field('restaurant_gallery')[0], 'medium', '800px'); ?>
+         <?php if (get_field('restaurant_signature')) {
+         ?>
+            <div class="signature">
+               <?= file_get_contents(get_field('restaurant_signature')); ?>
             </div>
-            <div class="shadow">
-               <?= Utils::imgLazy(get_field('restaurant_gallery')[1], 'medium', '800px'); ?>
+         <?php }
+         if (get_field('restaurant_gallery')[0]) {
+         ?>
+            <div class="shadow-image">
+               <div class="front">
+                  <?= Utils::imgLazy(get_field('restaurant_gallery')[0], 'medium', '800px'); ?>
+               </div>
+               <div class="shadow <?= (get_field('restaurant_gallery')[1] == null) ? 'lock' : ''; ?>">
+                  <?php if (get_field('restaurant_gallery')[1]) { ?>
+                     <?= Utils::imgLazy(get_field('restaurant_gallery')[1], 'medium', '800px'); ?>
+                  <?php } ?>
+               </div>
             </div>
-         </div>
+         <?php } ?>
       </div>
 
       <div class="side-image">
@@ -65,14 +73,39 @@ get_header(); ?>
          <div class="signature">
             <?= file_get_contents(get_field('chef_signature')); ?>
          </div>
-         <div class="shadow-image">
-            <div class="front">
-               <?= Utils::imgLazy(get_field('chef_gallery')[0], 'medium', '800px'); ?>
+         <?php if (get_field('chef_gallery')[0]) {
+         ?>
+            <div class="shadow-image">
+               <div class="front">
+                  <?= Utils::imgLazy(get_field('chef_gallery')[0], 'medium', '800px'); ?>
+               </div>
+               <div class="shadow <?= (get_field('chef_gallery')[1] == null) ? 'lock' : ''; ?>">
+                  <?php if (get_field('chef_gallery')[1]) { ?>
+                     <?= Utils::imgLazy(get_field('chef_gallery')[1], 'medium', '800px'); ?>
+                  <?php } ?>
+               </div>
             </div>
-            <div class="shadow">
-               <?= Utils::imgLazy(get_field('chef_gallery')[1], 'medium', '800px'); ?>
-            </div>
+         <?php } ?>
+      </div>
+   </div>
+</section>
+
+<section class="restaurant opening forest-blue">
+   <div class="container section-text-image">
+      <div class="side-image">
+         <div class="side-img">
+            <?php echo Utils::imgLazy(get_field('opening_gallery')[0], 'medium', '800px'); ?>
+            <?php echo Utils::imgLazy(get_field('opening_gallery')[1], 'medium', '800px'); ?>
+         </div>
+      </div>
+      <div class="side-text">
+         <h5><?= get_field('opening_pre_title'); ?></h5>
+         <h1><?= get_field('opening_title'); ?></h1>
+         <?= get_field('opening_text') ;?>
+         <div class="buttons">
+            <a href="<?= get_field('opening_button')['link']; ?>" class="linkBtn"><?= get_field('button_left')['label']; ?></a>
          </div>
       </div>
    </div>
 </section>
+
