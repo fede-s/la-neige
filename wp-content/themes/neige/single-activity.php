ff4e7eaa92d85421180f07ef104a4efec46f510b
DESKTOP-FTRQVEA\Tinma <xtinmax92@gmail.com>
2025-05-02 13:04:57 -0300

    activity info and gallery added

--- wp-content/themes/neige/single-activity.php
+++ wp-content/themes/neige/single-activity.php
@@ -1,11 +1,84 @@
 <?php
 
-global $post;
 
-$fields = get_fields($post);
-
-echo Utils::getSeasonField($fields, 'pre_title');
-
-
-
+get_header();
+$fields = get_fields();
+$gallery = Utils::getSeasonField($fields, 'gallery');
 ?>
+
+<div class="header activity-header">
+   <div class="gradient">
+      <?= Utils::imgLazyFromPost($post, 'large', '2000px'); ?>
+   </div>
+</div>
+<section class="container">
+   <div class="spacer"></div>
+   <div class="container">
+      <h1><?= the_title(); ?></h1>
+      <div class="activity-info">
+         <div class="info-left">
+            <?= Utils::getSeasonField($fields, 'intro') ?>
+         </div>
+         <div class="info-right">
+            <div class="property-features">
+               <div>
+                  <?= $fields['details'] ?>
+               </div>
+            </div>
+            <?php
+            if (!empty($fields['booking_title'])) { ?>
+               <div class="spacer"></div>
+               <div class="title-with-line">
+                  <span></span><?= $fields['booking_title']  ?><span></span>
+               </div>
+            <?php
+            }
+            if (!empty($fields['booking_button']['link'])) { ?>
+               <div class="book-button-wrapper">
+                  
+                     <a href="<?= $fields['booking_button']['link']; ?>" class="linkBtn"><?= $fields['booking_button']['label']; ?></a>
+                  
+               </div>
+            <?php
+            } ?>
+         </div>
+      </div>
+   </div>
+   <?php
+   if (!empty($gallery)) { ?>
+      <div class="spacer large"></div>
+      <div class="">
+         <?php Carousel::galleryFromACF($gallery); ?>
+      </div>
+   <?php
+   }
+   if (!empty($fields['sections']) && count($fields['sections'])) { ?>
+      <div class="spacer"></div>
+      <div class="container">
+         <?php Widgets::sections($fields['sections']) ?>
+      </div>
+   <?php
+   }
+   if (!empty($featured) && count($featured) > 0) { ?>
+      <div class="spacer large"></div>
+      <div class="container">
+         <h2 class="text-center"><?= $featuredTitle ?></h2>
+      </div>
+      <div class="container large">
+         <div class="spacer sm"></div>
+         <?php Carousel::generic($featured, $featuredTaxonomy, $season); ?>
+      </div>
+   <?php
+   }
+   if ($post->post_type === 'post') { ?>
+      <div class="spacer medium"></div>
+      <div class="text-center">
+         <a class="btn btn-primary btn-lg" href="javascript:window.history.back();">Back to News</a>
+      </div>
+   <?php
+   } ?>
+   <div class="spacer large"></div>
+
+</section>
+<?php
+get_footer();
