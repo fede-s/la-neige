629d14fe6f9494992a94ced62fbb28310e3d82b7
DESKTOP-FTRQVEA\Tinma <xtinmax92@gmail.com>
2025-05-05 05:51:02 -0300

    contact travel and map added

--- wp-content/themes/neige/page-contact.php
+++ wp-content/themes/neige/page-contact.php
@@ -19,6 +19,22 @@ get_header(); ?>
       <div class="sig-svg" style=" width:<?= $fields['signature_size']; ?>px">
          <?= file_get_contents($fields['signature']); ?>
       </div>
+      <div class="contact-data">
+         <?php
+         $tel = get_field('telephone', 'options');
+         if (!empty($tel)) { ?>
+            <div>
+               <p><a href="tel:<?= $tel ?>"><?= $tel; ?></a></p>
+            </div>
+         <?php } ?>
+         <?php
+         $mail = get_field('email', 'options');
+         if (!empty($mail)) { ?>
+            <div>
+               <p><a href="mailto:<?= $mail; ?>"><?= $mail; ?></a></p>
+            </div>
+         <?php } ?>
+      </div>
    </div>
    <div class="section-text-image">
       <div class="side-text">
@@ -40,6 +56,41 @@ get_header(); ?>
                </div>
             </div>
          <?php } ?>
+      </div>
+   </div>
+</section>
+
+<section class="contact container restaurant forest-blue access">
+   <div class="section-text-image">
+      <div class="side-text">
+         <div class="contact-title">
+            <h1><?= $fields['access_title']; ?></h1>
+            <div class="sig-svg" style=" width:<?= $fields['access_signature_size']; ?>px">
+               <?= file_get_contents($fields['access_signature']); ?>
+            </div>
+         </div>
+         <p class="access-text"><?= $fields['access_text'] ;?></p>
+         <?php 
+         foreach($fields['travel_options'] as $option) { ?>
+            <div class="travel-option">
+               <?= file_get_contents($option['icon']) ;?>
+               <h6><?= $option['title'] ;?></h6>
+               <p><?= $option['text'] ;?></p>
+            </div>
+
+
+         <?php } ?>
+      </div>
+
+      <div class="side-image">
+         <?php 
+         if($fields['map_image']) {
+            echo Utils::imgLazy($fields['map_image'], 'medium', '1000px');
+         } else {
+            echo $fields['map'];
+         }
+         ?>
+         
       </div>
    </div>
 </section>
