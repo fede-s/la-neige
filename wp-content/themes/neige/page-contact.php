4bd8c07f94de23ddc3b4ba99a294e082511b4a10
Fede <federico.suarez.87@gmail.com>
2025-05-09 14:15:46 -0300

    make all contact us page fields optional

--- wp-content/themes/neige/page-contact.php
+++ wp-content/themes/neige/page-contact.php
@@ -12,45 +12,42 @@ $fields = get_fields($post);
 get_header(); ?>
 
 
-
 <section class="contact container restaurant forest-blue">
    <div class="contact-title">
       <h1><?= $fields['title']; ?></h1>
-      <div class="sig-svg" style=" width:<?= $fields['signature_size']; ?>px">
-         <?= file_get_contents($fields['signature']); ?>
+      <div class="sig-svg">
+
       </div>
       <div class="contact-data">
          <?php
-         $tel = get_field('telephone', 'options');
-         if (!empty($tel)) { ?>
+         if (!empty($fields['tel'])) { ?>
             <div>
-               <p><a href="tel:<?= $tel ?>"><?= $tel; ?></a></p>
+               <p><a href="tel:<?= $fields['tel'] ?>"><?= $fields['tel']; ?></a></p>
             </div>
          <?php } ?>
          <?php
-         $mail = get_field('email', 'options');
-         if (!empty($mail)) { ?>
+         if (!empty($fields['email'])) { ?>
             <div>
-               <p><a href="mailto:<?= $mail; ?>"><?= $mail; ?></a></p>
+               <p><a href="mailto:<?= $fields['email'] ?>"><?= $fields['email'] ?></a></p>
             </div>
          <?php } ?>
       </div>
    </div>
    <div class="section-text-image">
       <div class="side-text">
-         <?= $fields['text']; ?>
+         <?= $fields['text'] ?? '' ?>
       </div>
 
       <div class="side-image">
          <?php
-         if ($fields['gallery'][0]) {
+         if (!empty($fields['gallery'][0])) {
          ?>
             <div class="shadow-image">
                <div class="front">
                   <?= Utils::imgLazy($fields['gallery'][0], 'medium', '800px'); ?>
                </div>
                <div class="shadow <?= ($fields['gallery'][1] == null) ? 'lock' : ''; ?>">
-                  <?php if ($fields['gallery'][1]) { ?>
+                  <?php if (!empty($fields['gallery'][1])) { ?>
                      <?= Utils::imgLazy($fields['gallery'][1], 'medium', '800px'); ?>
                   <?php } ?>
                </div>
@@ -64,33 +61,29 @@ get_header(); ?>
    <div class="section-text-image">
       <div class="side-text">
          <div class="contact-title">
-            <h1><?= $fields['access_title']; ?></h1>
-            <div class="sig-svg" style=" width:<?= $fields['access_signature_size']; ?>px">
-               <?= file_get_contents($fields['access_signature']); ?>
-            </div>
+            <h1><?= $fields['access_title'] ?? '' ?></h1>
          </div>
-         <p class="access-text"><?= $fields['access_text'] ;?></p>
-         <?php 
+         <p class="access-text"><?= $fields['access_text'] ?? '' ?></p>
+         <?php
+         if (!empty($fields['travel_options'])) {
          foreach($fields['travel_options'] as $option) { ?>
-            <div class="travel-option">
-               <?= Utils::imgLazy($option['icon'], 'large', '100px') ;?>
-               <h6><?= $option['title'] ;?></h6>
-               <p><?= $option['text'] ;?></p>
-            </div>
-
-
-         <?php } ?>
+             <div class="travel-option">
+                 <?= Utils::imgLazy($option['icon'], 'large', '100px') ;?>
+                 <h6><?= $option['title'] ;?></h6>
+                 <p><?= $option['text'] ;?></p>
+             </div>
+            <?php
+            }
+         } ?>
       </div>
 
       <div class="side-image">
          <?php 
-         if($fields['map_image']) {
+         if(!empty($fields['map_image'])) {
             echo Utils::imgLazy($fields['map_image'], 'medium', '1000px');
          } else {
-            echo $fields['map'];
-         }
-         ?>
-         
+            echo $fields['map'] ?? '';
+         } ?>
       </div>
    </div>
 </section>
