f073824635b493722c830a2090ca178ac4871946
DESKTOP-FTRQVEA\Tinma <xtinmax92@gmail.com>
2025-05-13 00:33:32 -0300

    gallery, pre title, title and signature added to features

--- wp-content/themes/neige/page-feature.php
+++ wp-content/themes/neige/page-feature.php
@@ -16,45 +16,59 @@ get_header(); ?>
       <?= Utils::imgLazy(get_field('header_image'), 'large', '2000px') ?>
    </div>
    <div class="header-text">
-      <h5><?= $fields['pre_title'] ;?></h5>
-      <h1><?= $fields['title'] ;?></h1>
+      <h5><?= $fields['pre_title']; ?></h5>
+      <h1><?= $fields['title']; ?></h1>
    </div>
 </div>
 <div class="introduction-text">
-   <p><?= $fields['introduction_text'] ;?></p>
-   <?php if(!empty($fields['introduction_signature'])) {
-    ?>
-   <div class="intro-signature">
-      <?= $fields['introduction_signature'] ;?>
-   </div>
-   <?php } ?>
- </div>
-
- <section class="container">
-   <?php if(!empty($fields['features'])) { ?>
-   <?php foreach($fields['features'] as $feature) { 
-      if (!empty($feature['image']) && !empty($feature['text'])) {
+   <p><?= $fields['introduction_text']; ?></p>
+   <?php if (!empty($fields['introduction_signature'])) {
    ?>
-   <div class="feature-image-text">
-      <div class="feature-image">
-      <?= Utils::imgLazy($feature['image'], 'large', '500px') ?>
+      <div class="intro-signature">
+         <?= $fields['introduction_signature']; ?>
       </div>
-      <div class="feature-text">
-      <?= $feature['text'] ;?>
-      </div>
+   <?php } ?>
+</div>
 
-   </div>
-   <?php }}}?>
+<section class="container">
+   <?php if (!empty($fields['features'])) { ?>
+      <?php foreach ($fields['features'] as $feature) {
+         if (!empty($feature['gallery']) && !empty($feature['text'])) {
+      ?>
+            <div class="feature-image-text">
+               <div class="feature-image">
+                  <?= Carousel::galleryFromACF($feature['gallery'], false, '', true);; ?>
+               </div>
+               <div class="feature-text">
+                  <h5><?= $feature['pre_title']; ?></h5>
+                  <div class="signature">
+                     <h1><?= $feature['title']; ?>
+                     <?php if(!empty($feature['signature'])) { ?>
+                        <div class="sig-svg">
+                           <?= $feature['signature']; ?>
+                        </div>
+                        <?php } ?>
+                     </h1>
+                  </div>
+                  <p>
+                     <?= $feature['text'] ;?>
+                  </p>
+               </div>
 
- </section>
+            </div>
+   <?php }
+      }
+   } ?>
 
- <?php if(!empty($fields['gallery'])) { ?>
- <section class="container">
- <?php Carousel::galleryFromACF($fields['gallery'], true, '', true); ?>
- </section>
+</section>
 
- <div class="spacer"></div>
- <?php } ?>
+<?php if (!empty($fields['gallery'])) { ?>
+   <section class="container">
+      <?php Carousel::galleryFromACF($fields['gallery'], true, '', true); ?>
+   </section>
+
+   <div class="spacer"></div>
+<?php } ?>
 
 
 <?php get_footer(); ?>
